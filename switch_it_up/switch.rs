d668bb25f69f7e7d51b23b3f267941752350f8b9
jtabalon <joseph@tabalon.io>
2022-08-13 08:05:04 -0700

    complete switch it up problem

--- switch_it_up/switch.rs
+++ switch_it_up/switch.rs
@@ -1,7 +1,21 @@
 fn switch_it_up(n: usize) -> &'static str {
-    todo!()
+    let result: &'static str = match n {
+        0 => "Zero",
+        1 => "One",
+        2 => "Two",
+        3 => "Three",
+        4 => "Four",
+        5 => "Five",
+        6 => "Six",
+        7 => "Seven",
+        8 => "Eight",
+        9 => "Nine",
+        _ => "Invalid",
+    };
+    result
 }
 
 fn main() {
-
+    let three: &'static str = switch_it_up(0);
+    println!("{}", three);
 }
